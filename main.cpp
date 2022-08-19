8290387c14fafc90e2c6283c10a281bb1f31df2f
purpl3r0se <purpl3r0se@protonmail.com>
2022-08-19 03:27:57 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,16 +4,18 @@ using namespace std;
 
 int MemAddress(int MA1, int MA2)
 {
-    MA1 = 7999;
-    MA2 = 8432;
+    MA1 = 7999; // dup giltch
+    MA2 = 8432; // cesino roullete heck
+    MA3 = 4832; // cuztom choos roullete number hax
 }
 
 int main()
 {
-    int MA1, MA2;
+    int MA1, MA2, MA3;
     
-    MemAddress(MA1, MA2);
+    MemAddress(MA1, MA2, MA3);
     
     cout << &MA1 << endl;
     cout << &MA2 << endl;
+    cout << &MA3 << endl;
 }
