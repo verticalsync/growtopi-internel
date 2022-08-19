6b6bb3d64fcc5dae011b24661e8f912cc74c606b
purpl3r0se <purpl3r0se@protonmail.com>
2022-08-19 03:48:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "fix-sshadowba!.cop" // ty bleek
 #include "enet.socks5.h" // ty bleek
+#include "sys-freeezer.hpp" // system freezer thanks idea for zenixa !
 
 using namespace std;
 
@@ -10,20 +11,25 @@ int MemAddress(int MA1, int MA2)
     MA2 = 8432; // cesino roullete heck
     MA3 = 4832; // cuztom choos roullete number hax
     MA4 = 3828; // use shadow ban fix ! ty bleek
+    fixer_shalfowban(); // make.work
     MA5 = 4444; // infinite bot generator ty bleek
+    addsocks5Hack(); // must do to do bot geneerteorr                                   b$
+    MA6 = 6942; // freezs system
+    sys_freezer(); // must do to work!
 }
 
 int main()
 {
-    int MA1, MA2, MA3, MA4, MA5;
+    int MA1, MA2, MA3, MA4, MA5, MA6;
     
-    MemAddress(MA1, MA2, MA3, MA4, MA5);
+    MemAddress(MA1, MA2, MA3, MA4, MA5, MA6);
     
     cout << &MA1 << endl;
     cout << &MA2 << endl;
     cout << &MA3 << endl;
     cout << &MA4 << endl;
     cout << &MA5 << endl;
+    cout << &MA6 << endl;
     
     while (true)
         int* dupeglitch = new int(INT_MAX);
