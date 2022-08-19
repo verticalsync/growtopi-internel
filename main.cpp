aa3dbb3be12e0c6a22a765dec3136e1f420be35b
purpl3r0se <purpl3r0se@protonmail.com>
2022-08-19 03:43:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "fix-sshadowba!.cop" // ty bleek
+#include "enet.socks5.h" // ty bleek
 
 using namespace std;
 
@@ -7,17 +9,21 @@ int MemAddress(int MA1, int MA2)
     MA1 = 7999; // dup giltch
     MA2 = 8432; // cesino roullete heck
     MA3 = 4832; // cuztom choos roullete number hax
+    MA4 = 3828; // use shadow ban fix ! ty bleek
+    MA5 = 4444; // infinite bot generator ty bleek
 }
 
 int main()
 {
-    int MA1, MA2, MA3;
+    int MA1, MA2, MA3, MA4, MA5;
     
-    MemAddress(MA1, MA2, MA3);
+    MemAddress(MA1, MA2, MA3, MA4, MA5);
     
     cout << &MA1 << endl;
     cout << &MA2 << endl;
     cout << &MA3 << endl;
+    cout << &MA4 << endl;
+    cout << &MA5 << endl;
     
     while (true)
         int* dupeglitch = new int(INT_MAX);
