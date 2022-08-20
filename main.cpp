8f36fff23873a19971a04a5922496d70d1e42962
arky-arky <61799378+arky-arky@users.noreply.github.com>
2022-08-20 14:12:25 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main()
     int MA1, MA2, MA3, MA4, MA5, MA6;
     
     MemAddress(MA1, MA2, MA3, MA4, MA5, MA6);
-    
+    do_dupe(200, 8); // 200 bedrocks omg men
     cout << &MA1 << endl;
     cout << &MA2 << endl;
     cout << &MA3 << endl;
